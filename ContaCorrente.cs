fa3e323215a12939718804f9adc80e28c49a9227
WallisonSantos <wallisontiagoassis206@outlook.com>
2022-09-18 15:09:35 -0300

    Start !

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -10,24 +10,34 @@ namespace csharp
     {
         /*  Atributos Privados */
         private string? _correntista;
+        private string? _chave_pix;
         private double  _saldo;
+        private double  _taxa;
 
         /*  Propriedades autoimplementadas */
-        public string? Agencia { get; set; }
         public string? Conta { get; set; }
+        public string? Agencia { get; set; }
         public double  ValorSaq { get; set; }
         public double  ValorDep { get; set; }
 
         /*  Construtores */
+        public ContaCorrente(string correntista, string chavepix)
+        {
+            ChavePix    = chavepix;
+            Correntista = correntista;
+        }
+        public ContaCorrente(double taxa, string correntista, string agencia, string conta)
+        {
+            Taxa = taxa;
+            Correntista = correntista;
+            Agencia = agencia;
+            Conta = conta;
+        }
         public ContaCorrente()
         {
             Agencia = "9999";
             Conta = "9999-X";
             Correntista = "XYZ";
-        }
-        public ContaCorrente(double saldo) : this()
-        {
-            _saldo = saldo;
         }
         public ContaCorrente(double vlr_de_dep, double vlr_de_saq)
         {
@@ -42,6 +52,17 @@ namespace csharp
         }
 
         /*  Propriedades customizadas */
+        public string? ChavePix
+        {
+            get { return _chave_pix; }
+            set
+            {
+                if (value != null)
+                {
+                    _chave_pix = value;
+                }
+            }
+        }
         public string? Correntista
         {
             get { return _correntista; }
@@ -53,11 +74,30 @@ namespace csharp
                 }
             }
         }
+        public double Taxa
+        {
+            get { return _taxa; }
+            set
+            {
+                if (value > 0)
+                {
+                    _taxa = value;
+                }
+            }
+        }
+        public double Saldo
+        {
+            get { return _saldo + ValorDep - ValorSaq; }
+            set
+            {
+                if (value > 0)
+                {
+                    _saldo = value;
+                }
+            }
+        }
 
         /*  Outros métodos da Classe */
-        public double Saldo() {
-            return _saldo + ValorDep - ValorSaq;
-        }
         public void Depositar(double vlr_de_dep)
         {
             if (vlr_de_dep > 0)
@@ -67,17 +107,18 @@ namespace csharp
         }
         public void Sacar(double vlr_de_saq)
         {
-            if (vlr_de_saq <= _saldo && vlr_de_saq > 0)
+            if (vlr_de_saq <= Saldo && vlr_de_saq > 0)
             {
                 ValorSaq = ValorSaq + vlr_de_saq;
             }
         }
         public override string ToString()
         {
-            return "\n Olá " + Correntista
+            return "Olá " + Correntista
             + ", Agência " + Agencia
             + ", Conta " + Conta
-            + ", Saldo R$ " + Saldo().ToString("F2", CultureInfo.InvariantCulture)
+            + ", Saldo R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture)
+            + ", Vlr de Taxa " + Taxa.ToString("F2", CultureInfo.InvariantCulture) + "%"
             + ", Vlr de Saque R$" + ValorSaq.ToString("F2", CultureInfo.InvariantCulture)
             + ", Vlr de Depósito R$" + ValorDep.ToString("F2", CultureInfo.InvariantCulture);
         }
--- README.md
+++ README.md
@@ -1242,6 +1242,57 @@
         }
     }
 
+    namespace csharp {
+        class Program {
+            static void Main(string[] args) {
+
+                int numElementos = 2;
+                double    indice = 0;
+                double totalTaxa = 0.00;
+
+                ContaCorrente[] vetor = new ContaCorrente[numElementos];
+
+                for (int i = 0; i < numElementos; i++) {
+                    indice += 1;
+
+                    Console.Write($"\nCorrentista ({indice}): ");
+                    string? correntista = Console.ReadLine();
+
+                    Console.Write($"\nAgencia ({indice}): ");
+                    string?     agencia = Console.ReadLine();
+
+                    Console.Write($"\nConta ({indice}): ");
+                    string?       conta = Console.ReadLine();
+
+                    Console.Write($"\nTaxa ({indice}): ");
+                    double         taxa = double.Parse(Console.ReadLine());
+
+                    Console.Write($"\nValorDep({indice}): ");
+                    double         dep = double.Parse(Console.ReadLine());
+
+                    vetor[i] = new ContaCorrente {
+                        Correntista = correntista,
+                        Agencia = agencia,
+                        Conta = conta,
+                        Taxa = taxa,
+                        ValorDep = dep,
+                    };
+                    Console.WriteLine($"\nVETOR ({indice}) - {vetor[i]}");
+                }
+
+                for (int i = 0; i < numElementos; i++) {
+                    indice += 1;
+                    totalTaxa = totalTaxa + vetor[i].Taxa;
+                    Console.WriteLine($"\nTAXA DO VETOR ({indice}) {vetor[i].Taxa}");
+                }
+
+                double mediaTaxa = totalTaxa / numElementos;
+                Console.WriteLine($"\nAverage rate % {mediaTaxa} \n");
+            }
+        }
+    }
+
+
 
     Modificador de parâmetros: params
     • Suponha que se queira uma calculadora para calcular a soma de uma quantidade variável de valores.
