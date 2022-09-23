7f4b2a1b077cefdec583caddbff3c3f1398d58b4
WallisonSantos <wallisontiagoassis206@outlook.com>
2022-09-23 13:08:18 -0300

    CS

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -6,161 +6,162 @@ using System.Collections.Generic;
 
 namespace csharp
 {
-    public class ContaCorrente : IEquatable<ContaCorrente>
-    {
-        /*  Atributos Privados */
-        private string? _correntista;
-        private int _chave_pix;
-        private double _saldo;
-        private double _taxa;
+   public class ContaCorrente : IEquatable<ContaCorrente>
+   {
+      /*  Atributos Privados */
+      private string? _correntista;
+      private int _chave_pix;
+      private double _saldo;
+      private double _taxa;
 
-        /*  Propriedades autoimplementadas */
-        public string? Conta { get; set; }
-        public string? Agencia { get; set; }
-        public double ValorSaq { get; set; }
-        public double ValorDep { get; set; }
-        private static double _total_aplicacao { get; set; }
+      /*  Propriedades autoimplementadas */
+      public string? Conta { get; set; }
+      public string? Agencia { get; set; }
+      public double ValorSaq { get; set; }
+      public double ValorDep { get; set; }
+      private static double _total_aplicacao { get; set; }
 
-        /*  Construtores */
-        public ContaCorrente(string correntista, int chavepix)
-        {
-            ChavePix = chavepix;
-            Correntista = correntista;
-        }
-        public ContaCorrente(double taxa, string correntista, string agencia, string conta)
-        {
-            Taxa = taxa;
-            Correntista = correntista;
-            Agencia = agencia;
-            Conta = conta;
-        }
-        public ContaCorrente()
-        {
-            Agencia = "9999";
-            Conta = "9999-X";
-            Correntista = "XYZ";
-        }
-        public ContaCorrente(double vlr_de_dep, double vlr_de_saq)
-        {
-            Depositar(vlr_de_dep);
-            Sacar(vlr_de_saq);
-        }
-        public ContaCorrente(double vlr_de_dep, double vlr_de_saq, string correntista, string agencia, string conta) : this(vlr_de_dep, vlr_de_saq)
-        {
-            Correntista = correntista;
-            Agencia = agencia;
-            Conta = conta;
-        }
+      /*  Construtores */
+      public ContaCorrente(string correntista, int chavepix)
+      {
+         ChavePix = chavepix;
+         Correntista = correntista;
+      }
+      public ContaCorrente(double taxa, string correntista, string agencia, string conta)
+      {
+         Taxa = taxa;
+         Correntista = correntista;
+         Agencia = agencia;
+         Conta = conta;
+      }
+      public ContaCorrente()
+      {
+         Agencia = "9999";
+         Conta = "9999-X";
+         Correntista = "XYZ";
+      }
+      public ContaCorrente(double vlr_de_dep, double vlr_de_saq)
+      {
+         Depositar(vlr_de_dep);
+         Sacar(vlr_de_saq);
+      }
+      public ContaCorrente(double vlr_de_dep, double vlr_de_saq, string correntista, string agencia, string conta) : this(vlr_de_dep, vlr_de_saq)
+      {
+         Correntista = correntista;
+         Agencia = agencia;
+         Conta = conta;
+      }
 
-        /*  Propriedades customizadas */
-        public int ChavePix
-        {
-            get { return _chave_pix; }
-            set
+      /*  Propriedades customizadas */
+      public int ChavePix
+      {
+         get { return _chave_pix; }
+         set
+         {
+            if (value > 0)
             {
-                if (value > 0)
-                {
-                    _chave_pix = value;
-                }
+               _chave_pix = value;
             }
-        }
-        public string? Correntista
-        {
-            get { return _correntista; }
-            set
+         }
+      }
+      public string? Correntista
+      {
+         get { return _correntista; }
+         set
+         {
+            if (value != null)
             {
-                if (value != null)
-                {
-                    _correntista = value;
-                }
+               _correntista = value;
             }
-        }
-        public double Taxa
-        {
-            get { return _taxa; }
-            set
+         }
+      }
+      public double Taxa
+      {
+         get { return _taxa; }
+         set
+         {
+            if (value > 0)
             {
-                if (value > 0)
-                {
-                    _taxa = value;
-                }
+               _taxa = value;
             }
-        }
-        public double Saldo
-        {
-            get { return _saldo + ValorDep - ValorSaq; }
-            set
+         }
+      }
+      public double Saldo
+      {
+         get { return _saldo + ValorDep - ValorSaq; }
+         set
+         {
+            if (value > 0)
             {
-                if (value > 0)
-                {
-                    _saldo = value;
-                }
+               _saldo = value;
             }
-        }
+         }
+      }
 
-        /*  Outros métodos da Classe */
-        public void Depositar(double vlr_de_dep)
-        {
-            if (vlr_de_dep > 0)
-            {
-                ValorDep = ValorDep + vlr_de_dep;
-            }
-        }
-        public void Sacar(double vlr_de_saq)
-        {
-            if (vlr_de_saq <= Saldo && vlr_de_saq > 0)
-            {
-                ValorSaq = ValorSaq + vlr_de_saq;
-            }
-        }
+      /*  Outros métodos da Classe */
+      public void Depositar(double vlr_de_dep)
+      {
+         if (vlr_de_dep > 0)
+         {
+            ValorDep = ValorDep + vlr_de_dep;
+         }
+      }
+      public void Sacar(double vlr_de_saq)
+      {
+         if (vlr_de_saq <= Saldo && vlr_de_saq > 0)
+         {
+            ValorSaq = ValorSaq + vlr_de_saq;
+         }
+      }
 
-        public override bool Equals(object? obj)
-        {
-            if ( obj == null ) return false;
-            ContaCorrente? objAsConta = obj as ContaCorrente;
-            if ( objAsConta == null ) return false;
-            else return Equals(objAsConta);
-        }
+      public override bool Equals(object? obj)
+      {
+         if (obj == null) return false;
+         ContaCorrente? objAsConta = obj as ContaCorrente;
+         if (objAsConta == null) return false;
+         else return Equals(objAsConta);
+      }
 
-        public override int GetHashCode()
-        {
-            return ChavePix;
-        }
+      public override int GetHashCode()
+      {
+         return ChavePix;
+      }
 
-        public bool Equals(ContaCorrente? other) {
-            if ( other == null ) return false;
-            return ( ChavePix.Equals( other.ChavePix));
-        }
+      public bool Equals(ContaCorrente? other)
+      {
+         if (other == null) return false;
+         return (ChavePix.Equals(other.ChavePix));
+      }
 
-        public override string ToString()
-        {
-            return "Olá " + Correntista+"X"+ChavePix
-            + ", Agência " + Agencia
-            + ", Conta " + Conta
-            + ", Saldo R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture)
-            + ", Vlr de Taxa " + Taxa.ToString("F2", CultureInfo.InvariantCulture) + "%"
-            + ", Vlr de Saque R$" + ValorSaq.ToString("F2", CultureInfo.InvariantCulture)
-            + ", Vlr de Depósito R$" + ValorDep.ToString("F2", CultureInfo.InvariantCulture);
-        }
+      public override string ToString()
+      {
+         return "Olá " + Correntista + "X" + ChavePix
+         + ", Agência " + Agencia
+         + ", Conta " + Conta
+         + ", Saldo R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture)
+         + ", Vlr de Taxa " + Taxa.ToString("F2", CultureInfo.InvariantCulture) + "%"
+         + ", Vlr de Saque R$" + ValorSaq.ToString("F2", CultureInfo.InvariantCulture)
+         + ", Vlr de Depósito R$" + ValorDep.ToString("F2", CultureInfo.InvariantCulture);
+      }
 
-        // Função que irá receber uma quantidade variável de valores para soma
-        // Usando vetor na passagem de parametro e iterando sobre esse mesmo vetor aplicando a soma da variável _total_aplicacao + numbers(na posição atual do vetor)
-        public static double Sum(params double[] numbers)
-        {
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                _total_aplicacao = _total_aplicacao + numbers[i];
-            }
-            return _total_aplicacao;
-        }
-        // Uso de Modificadores de parâmetros ref e out
-        public static void Sum2(ref double x)
-        {
-            x = x + 1.20;
-        }
-        public static void Mult(double origin, out double result)
-        {
-            result = origin * 1.20;
-        }
-    }
+      // Função que irá receber uma quantidade variável de valores para soma
+      // Usando vetor na passagem de parametro e iterando sobre esse mesmo vetor aplicando a soma da variável _total_aplicacao + numbers(na posição atual do vetor)
+      public static double Sum(params double[] numbers)
+      {
+         for (int i = 0; i < numbers.Length; i++)
+         {
+            _total_aplicacao = _total_aplicacao + numbers[i];
+         }
+         return _total_aplicacao;
+      }
+      // Uso de Modificadores de parâmetros ref e out
+      public static void Sum2(ref double x)
+      {
+         x = x + 1.20;
+      }
+      public static void Mult(double origin, out double result)
+      {
+         result = origin * 1.20;
+      }
+   }
 }
--- Program.cs
+++ Program.cs
@@ -3,42 +3,64 @@ using System.Globalization;
 using System.Collections.Generic;
 namespace csharp
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Criando a lista de parts.
-            List<ContaCorrente> Conta = new List<ContaCorrente>();
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         // Criando a lista de parts.
+         List<ContaCorrente> Conta = new List<ContaCorrente>();
 
-            Conta.Add(new ContaCorrente() { Correntista = "WTA", ChavePix = 00009 });
-            Conta.Add(new ContaCorrente() { Correntista = "KAS", ChavePix = 00010 });
-            Conta.Add(new ContaCorrente() { Correntista = "MAA", ChavePix = 00011 });
-            Conta.Add(new ContaCorrente() { Correntista = "AAS", ChavePix = 00012 });
+         Conta.Add(new ContaCorrente() { Correntista = "WTA", ChavePix = 00009 });
+         Conta.Add(new ContaCorrente() { Correntista = "KAS", ChavePix = 00010 });
+         Conta.Add(new ContaCorrente() { Correntista = "MA", ChavePix = 00011 });
+         Conta.Add(new ContaCorrente() { Correntista = "AS", ChavePix = 00012 });
 
-            Conta.Insert(2, new ContaCorrente() { Correntista = "JGF", ChavePix = 00013 });
+         Conta.Insert(2, new ContaCorrente() { Correntista = "JGF", ChavePix = 00013 });
 
-            foreach (ContaCorrente contas in Conta)
-            {
-                System.Console.WriteLine($"Contas: {contas}");
-            }
-            System.Console.WriteLine();
-
-
-            for (int i = 0; i < Conta.Count; i++)
-            {
-                System.Console.WriteLine($"Conta Corrente: {Conta[i]}");
-            }
-            System.Console.WriteLine("\nNúmero de contas: {0}\n", Conta.Count);
+         foreach ( ContaCorrente contas in Conta )
+         {
+            Console.WriteLine($"Contas: {contas}");
+         }
+         Console.WriteLine();
 
 
-            Console.WriteLine("\nCONTAINS Chave Pix: {0}",
+         for ( int i = 0; i < Conta.Count; i++ )
+         {
+            Console.WriteLine($"Conta Corrente: {Conta[i]}");
+         }
+         Console.WriteLine("\nNúmero de contas: {0}\n", Conta.Count);
+
+
+         Console.WriteLine("\nCONTAINS Chave Pix: {0}",
             Conta.Contains(new ContaCorrente { Correntista = "WTA", ChavePix = 00009 }));
 
-            Console.WriteLine("\nFIND Correntista where correntista contains \"WTA\":  {0}",
+         Console.WriteLine("\nFIND Correntista where correntista contains \"WTA\":  {0}",
             Conta.Find(cc => cc.Correntista.Contains("WTA")));
 
-            Console.WriteLine("\nEXISTS chave pix with 00013: {0} \n",
+         Console.WriteLine("\nEXISTS chave pix with 00013: {0} \n",
             Conta.Exists(cc => cc.ChavePix == 00013));
-        }
-    }
+
+         Console.WriteLine("\nFIND LAST Correntsta where correntista contais \"A\": {0}",
+            Conta.FindLast(cc => cc.Correntista.Contains("A")));
+
+         Console.WriteLine("\nFIND INDEX Correntsta where correntista contais \"A\": {0}",
+            Conta.FindIndex(cc => cc.Correntista.Contains("A")));
+
+         Console.WriteLine("\nFIND LAST INDEX Correntsta where correntista contais \"A\": {0}",
+            Conta.FindLastIndex(cc => cc.Correntista.Contains("A")));
+            Console.WriteLine();
+
+         List<ContaCorrente> ContaFilter = Conta.FindAll(conta => conta.Correntista.Length == 3);
+
+         foreach( ContaCorrente filter in ContaFilter ) {
+            Console.WriteLine("FILTRO contas com 3 letras {0}", filter);
+         }
+
+         Conta.RemoveAll(conta => conta.Correntista.Length == 3);
+
+         foreach( ContaCorrente contas in Conta ) {
+            Console.WriteLine("Todas as Contas {0}", contas);
+         }
+      }
+   }
 }
